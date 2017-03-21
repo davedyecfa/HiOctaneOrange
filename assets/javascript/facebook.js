56a1a9cd6dc8f67c2adbed67d544f78a7b40561f
usmanjamil83 <usmanjamil83@hotmail.com>
2017-03-21 19:03:19 -0400

    search resultworking

--- assets/javascript/facebook.js
+++
@@ -1,8 +0,0 @@
-$(".fb-share-button").on("click", function (){
-    FB.ui(
- {
-  method: 'share',
-  href: 'https://developers.facebook.com/docs/'
-}, function(response){});
-});
-
--- assets/javascript/ticketmaster.js
+++ assets/javascript/ticketmaster.js
@@ -1,37 +1,81 @@
-    // var startDateTime=2017-03-17;
-    // var endDateTime=2017-03-17;
-    // var keyword = "atlanta";
+$(document).ready(function(){
+  $("#events-panel").hide();
 
-    $.ajax({
-      type:"GET",
-      url:"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&keyword=atlanta&endDateTime=2017-03-17T17:00:00Z&apikey=3d6mOOKQJKcLtzdmYGpP5rdw45kOwVKG",
-      async:true,
-      dataType: "json",
+  var page = 0;
 
-      success: function(json) {
-        //console.log(json);
-        var results = json._embedded.events;
-        console.log(results);
+  function getEvents(page) {
 
-        for (var i = 0; i < results.length; i++) {
+    if (page < 0) {
+      page = 0;
+      return;
+    }
+    if (page > 0) {
+      if (page > getEvents.json.page.totalPages-1) {
+        page=0;
+      }
+    }
 
-          var info = $("<div>");
+    $("#search-button").on("click", function() {
+      $("#events-panel").show();
+      var city = $("#city").val();
+      console.log("city" + city);
 
-          var name = $("<h3>").text(results[i].name);
-
-          //var price = $("<p>").text(results[i].priceRanges[i].min);
-
-          var dates = $("<p>").text(results[i].dates.start.localDate);
-
-          info.append(name, price, dates);
-
-          $("#name").prepend(info);
+      $.ajax({
+        type:"GET",
+        url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey=3d6mOOKQJKcLtzdmYGpP5rdw45kOwVKG&classificationName=music&keyword=" + city + "&size=20&page="+page,
+        async:true,
+        dataType: "json",
+        success: function(json) {
+          console.log(json);
+          getEvents.json = json;
+          showEvents(json);
+        },
+        error: function(xhr, status, err) {
+          console.log(err);
         }
-      },
+      });
     });
+  }
 
-// &keyword=atlanta
-// &startDateTime=2017-03-17T17:00:00Z
-// &endDateTime=2017-03-17T17:00:00Z
-// only use start date or end date it does not work
-// with both and i think start date will be just fine
+  function showEvents(json) {
+    var items = $("#events .list-group-item");
+    items.hide();
+    var events = json._embedded.events;
+    var item = items.first();
+    for (var i=0;i<events.length;i++) {
+      if (events[i]._embedded.attractions !== undefined){
+        item.children('.list-group-item-heading').text(events[i]._embedded.attractions[0].name);
+      }
+      item.children('.list-group-item-text').text(events[i].dates.start.localDate);
+      if (events[i].priceRanges !== undefined){
+        $(".price").text(events[i].priceRanges[0].min);
+      }
+      try {
+        item.children(".venue").text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
+      } catch (err) {
+        console.log(err);
+      }
+      item.show();
+      item.off("click");
+      item.click(events[i], function(eventObject) {
+        console.log(eventObject.data);
+        try {
+          getAttraction(eventObject.data._embedded.attractions[0].id);
+        } catch (err) {
+          console.log(err);
+        }
+      });
+      item=item.next();
+    }
+  }
+
+  $("#prev").click(function() {
+    getEvents(--page);
+  });
+
+  $("#next").click(function() {
+    getEvents(++page);
+  });
+
+  getEvents(page);
+});
